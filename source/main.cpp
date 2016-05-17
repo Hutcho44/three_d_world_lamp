7a285f887227212d726d1c63e4c289c15b7dedd8
Ben Hutchings <Hutcho@Bens-MacBook-Pro.local>
2016-05-17 13:57:21 +1000

    initial commit

---
+++ source/main.cpp
@@ -0,0 +1,11 @@
+//
+//  main.cpp
+//  three_d_world_lamp
+//
+//  Created by Ben Hutchings on 17/05/2016.
+//  Copyright © 2016 Ben Hutchings. All rights reserved.
+//
+
+int main() {
+    return(0);
+}
